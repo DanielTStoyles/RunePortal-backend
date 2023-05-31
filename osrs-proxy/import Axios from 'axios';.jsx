d6e5518b200dcb25d08f8f7ada487448638719ea
DanielTStoyles <daniel_stoyles@live.ca>
2023-05-31 14:52:04 -0230

    create item database in JSON

---
+++ osrs-proxy/import Axios from 'axios';.jsx
@@ -0,0 +1,6 @@
+/** @format */
+
+// import Axios from 'axios';
+// import React from 'react';
+
+// const getItemList = async ()
--- osrs-proxy/proxy-server.js
+++ osrs-proxy/proxy-server.js
@@ -3,6 +3,7 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors";
+import fs from "fs";
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,7 @@ const OSRS_BASE_URL =
   "https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws";
 
 const OSRS_GE_BASE_URL = "https://prices.runescape.wiki/api/v1/osrs/mapping";
+
 app.get("/api/playerStats/:playerName", async (req, res) => {
   try {
     const { playerName } = req.params;
@@ -24,17 +26,19 @@ app.get("/api/playerStats/:playerName", async (req, res) => {
   }
 });
 
-app.get("/api/item/:itemId", async (req, res) => {
-  try {
-    const { itemId } = req.params;
-    const response = await axios.get(
-      `${OSRS_GE_BASE_URL}?item=${encodeURIComponent(itemId)}`
-    );
-    res.send(response.data);
-  } catch (error) {
-    res.status(500).send("Error fetching item data: ${error.message}");
-  }
-});
+axios.get(OSRS_GE_BASE_URL)
+  .then((response) => {
+    fs.writeFile("items.json", JSON.stringify(response.data), (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+      } else {
+        console.log("File written successfully.");
+      }
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
